3d7232446f500dffb25d3f0331e7bc9eb70c7c5a
octavioduartejurczyszyn <tatoduarte9@gmail.com>
2024-02-20 19:07:08 +0100

    Primera subida

---
+++ Ejemplo1-Último/Perro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo1_Último
+{
+    internal class Perro
+    {
+            private string nombre;
+            private string color;
+            private string origen;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public string Origen
+        { 
+            get { return origen; } 
+            set { origen = value; }
+        }
+
+    }
+}
---
+++ Ejemplo1-Último/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo1_Último
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Perro p1 = new Perro();
+            p1.Nombre = "ringo";
+            p1.Origen = "pichcichu";
+            p1.Color = "blanco y marrón";
+
+            Console.WriteLine("El nombre del perro es " + p1.Nombre + ", su origen " + p1.Origen + " y su color es " + p1.Color);
+            Console.ReadKey();
+
+        }
+    }
+}
